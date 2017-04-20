a23a23bab4446d8d192cc927798bfac8059ac9ac
16sheep <marjutubli@gmail.com>
2017-04-20 22:12:11 +0100

    Configure express middleware

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
+//Load in node modules
 var express = require('express');
-var session = require('express-session');
-var passport = require('passport');
-var LocalStrategy = require('passport-local');
-var routes = require("routes")
 var bodyparser = require('body-parser');
 var mongoose = require('mongoose');
-var react = require('react');
 
-mongoose.connect('mongodb://localhost/users');
-var database = mongoose.connection;
+//Create database connection string
+var db = 'mongodb://localhost:27017/users';
 
+//Create express app
 var app = express();
 
+mongoose.connect(db, (err) => {
+     if(err){
+         console.log(err)
+     }
+ });
 
-
+//Configure express middleware
+app.use(bodyparser.json);
+app.use(express.static(__dirname + '/public'))
+app.get("*", (request, response) => {
+    response.sendFile(__dirname + '/public.index.html')
+})
+//Create server
 app.listen(process.env.PORT || 3000, () => {
     console.log("App is Working")
 });
